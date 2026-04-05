c55f98a456a35efa471f33c113e88021dc2c8b01
Adam Chubbuck <adam.chubbuck@gmail.com>
2026-04-05 01:43:02 -0400

    Add API bootstrap

---
+++ apps/api/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(3000);
+  console.log('API listening on http://localhost:3000');
+}
+
+bootstrap();
